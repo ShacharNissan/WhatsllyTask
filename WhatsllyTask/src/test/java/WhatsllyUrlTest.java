6ee86654ff629d255b2dbe470b96d284e4b96d18
"Shachar Nissan" <Shachar208@gmail.com>
2021-06-29 17:35:29 +0300

    Load test

--- WhatsllyTask/src/test/java/WhatsllyUrlTest.java
+++ WhatsllyTask/src/test/java/WhatsllyUrlTest.java
@@ -17,39 +17,39 @@ class WhatsllyUrlTest {
 	private String START_URL = "https://api-";
 	private String VALID_END_URL = ".whatslly.com/test/ping.json"; // Valid and good Whatslly's URL.
 	private String INVALID_END_URL = ".whatslly.com/test"; // Invalid Whatslly's URL.
-	private String[] REGIONS = {"us1", "br1", "eu1", "st1"};
-	
+	private String[] REGIONS = { "us1", "br1", "eu1", "st1" };
+
 	private String JSON_SUCCESS_KEY = "success";
-	private String JSON_HOST_KEY = "host";
-	private String JSON_HOST_VALUE_OK = ".whatslly.com";
 	private boolean JSON_SUCCESS_VALUE_OK = true;
-	
+
 	private int OK_RESPONSE_CODE = 200;
 	private int NOT_FOUND_RESPONSE_CODE = 404;
 	
+	private int NUMBER_OF_TESTS = 350;
+
 	/*
 	 * Test each of available regions in REGIONS array for valid response.
-	 * URL inputs: 	https://api-us1.whatslly.com/test/ping.json,
-	 *  			https://api-br1.whatslly.com/test/ping.json,
-	 *  			https://api-eu1.whatslly.com/test/ping.json,
-	 *  			https://api-st1.whatslly.com/test/ping.json
+	 * URL inputs: https://api-us1.whatslly.com/test/ping.json,
+	 * https://api-br1.whatslly.com/test/ping.json,
+	 * https://api-eu1.whatslly.com/test/ping.json,
+	 * https://api-st1.whatslly.com/test/ping.json 
 	 * Expected results : Response code 200.
 	 */
 	@Test
 	void testUrlAllRegionsOk() {
 		String url = "";
-		for (int i = 0; i < REGIONS.length ; i++) {
+		for (int i = 0; i < REGIONS.length; i++) {
 			url = START_URL + REGIONS[i] + VALID_END_URL;
-			
+
 			try {
 				int code = getResponseCode(url);
 				assertEquals(OK_RESPONSE_CODE, code);
-			}catch (Exception e) {
+			} catch (Exception e) {
 				fail("Test testUrlAllRegionsOk - failed to load URL = " + url);
 			}
 		}
 	}
-	
+
 	/*
 	 * Test one of available regions in REGIONS array for invalid response.
 	 * URL input: https://api-us1.whatslly.com/test
@@ -62,11 +62,11 @@ class WhatsllyUrlTest {
 		try {
 			code = getResponseCode(url);
 			assertEquals(NOT_FOUND_RESPONSE_CODE, code);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			fail("Test testUrlInvalid - unexpected error occurred.");
 		}
 	}
-	
+
 	/*
 	 * Gets json from URL and validate JSON_SUCCESS_KEY ("success") key.
 	 * URL input: https://api-us1.whatslly.com/test/ping.json
@@ -78,26 +78,31 @@ class WhatsllyUrlTest {
 		try {
 			JSONObject json = readJsonFromUrl(url);
 			assertEquals(JSON_SUCCESS_VALUE_OK, json.get(JSON_SUCCESS_KEY));
-		}catch (Exception e) {
+		} catch (Exception e) {
 			fail("Test testSuccessStatusValid - failed to validate.");
 		}
 	}
-	
+
 	/*
-	 * Gets json from URL and validate JSON_HOST_KEY ("host") key.
-	 * URL input: https://api-us1.whatslly.com/test/ping.json
-	 * Expected result : START_URL + REGIONS[0] + JSON_HOST_VALUE_OK (https://api-us1.whatslly.com).
+	 * Preform a load test to the server with NUMBER_OF_TESTS (350) api requests.
+	 * input: URL-https://api-us1.whatslly.com/test/ping.json,  NUMBER_OF_TESTS=350
+	 * Expected result : true (all tests - OK)
 	 */
 	@Test
-	void testHostStatusValid() {
+	void testLoad() {
 		String url = START_URL + REGIONS[0] + VALID_END_URL;
-		String testUrl = START_URL + REGIONS[0] + JSON_HOST_VALUE_OK;
-		try {
-			JSONObject json = readJsonFromUrl(url);
-			assertEquals(testUrl, json.get(JSON_HOST_KEY));
-		}catch (Exception e) {
-			fail("Test testHostStatusValid - failed to validate.");
+		int code = 0;
+		boolean flag = true;
+		for (int i = 0; i < NUMBER_OF_TESTS; i++) {
+			try {
+				code = getResponseCode(url);
+				if (code != OK_RESPONSE_CODE)
+					flag = false;
+			} catch (Exception e) {
+				fail("Test testHostStatusValid - failed to validate.");
+			}
 		}
+		assertEquals(true, flag);
 	}
 
 	// Takes all Input received in rd (Reader) and load it into a single String.
@@ -122,11 +127,11 @@ class WhatsllyUrlTest {
 			is.close();
 		}
 	}
-	
+
 	// Given a URL, return status / response code.
 	private int getResponseCode(String url) throws IOException {
 		URL urlConnection = new URL(url);
-		HttpURLConnection connection = (HttpURLConnection)urlConnection.openConnection();
+		HttpURLConnection connection = (HttpURLConnection) urlConnection.openConnection();
 		connection.setRequestMethod("GET");
 		connection.connect();
 
